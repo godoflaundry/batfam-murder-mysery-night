8933d27a9a82bd02e2d6cbf52e44959800157c0b
godoflaundry <56318296+godoflaundry@users.noreply.github.com>
2022-03-16 23:32:07 -0500

    animation setup

---
+++ README.md
@@ -0,0 +1,17 @@
+# batfam murder mystery night 
+
+This repository holds the code that runs the animation for the ficlet: batman murder mystery night
+
+You can find the fic on  [tumblr](https://www.google.com/url?q=https://godoflaundrybaskets.tumblr.com/post/674625251681976320&sa=D&source=docs&ust=1647494255164677&usg=AOvVaw3YgnnGg3O1_7p_yvyuJf7X). 
+
+## I want to read the source code.
+
+This repository is adapted from of oakleaf's project on [github]{https://github.com/justoakleaf/kay2-has-entered-chat}
+
+## I want to read the source code.
+
+You can clone the project and then view the files inside `docs`. There are no external dependencies.
+
+Files in the `helper-scripts` directory are helpers to assist in creating the html file. They are stored here for the developer's benefit.
+
+After cloning/downloading, open `docs/index.html` in your browser to view the animation.
---
+++ helper-scripts/htmlmaker.py
@@ -0,0 +1,65 @@
+SCRIPT_PATH = r'script.txt'
+TIMINGS_PATH = r'timings.txt'
+HTML_LINES_PATH = r'htmllines.txt'
+
+import html
+
+with open(SCRIPT_PATH, 'rb') as fh:
+    tmplines = fh.read()
+
+with open(TIMINGS_PATH) as fh:
+    timinglines = fh.readlines()
+
+tmplines = tmplines.decode('utf-8').replace('’', "'").replace('“', '"').replace("”", '"')
+scriptlines = tmplines.splitlines()
+
+htmllines = []
+
+override_typing = [161, 195, 199]
+
+for i, line in enumerate(scriptlines):
+    cleanline = line.strip()
+
+    if len(cleanline) < 1:
+        # skip empty lines
+        continue
+
+    typeStart = 0
+    user = ''
+    if cleanline.startswith('>'):
+        # K2 command
+        typeStart = 3
+        lineType = 'K2cmd'
+        prompt = '>>'
+    elif cleanline.startswith('.'):
+        typeStart = 3
+        lineType = 'K2cmd'
+        prompt = '..'
+    elif cleanline.startswith(':'):
+        lineType = 'DCctrl'
+        prompt = "Kay2>"
+    else:
+        # message line
+        lineType = 'msg'
+        msg = cleanline.split(':',1)
+        if msg[0] == '3POh':
+            spanclass = 'TPOh'
+        else:
+            spanclass=msg[0]
+        user = f'<div><span class="username {spanclass}">{msg[0]}</span></div>'
+        cleanline = msg[1].strip()
+        prompt = "Kay2>"
+
+    if i in override_typing:
+        typeStart = len(cleanline)
+
+    linedata = html.escape(cleanline).replace('  ', '&nbsp;&nbsp;')
+
+    divAttrs = f'id="msg{i}" class="message-container" data-class="{lineType}" data-timing="{timinglines[i].strip()}" data-type-start="{typeStart}" data-prompt="{prompt}"'
+
+    htmlline = f'<div {divAttrs}><div class="message-space">{user}<div>{linedata}</div></div></div>\n'
+
+    htmllines.append(htmlline)
+
+with open(HTML_LINES_PATH, 'w+') as fh:
+    fh.writelines(htmllines)
